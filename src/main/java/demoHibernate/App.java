92618c3533d14eb484b410296e9fc76336b209ca
Gautam Chakraborty <gchakrborty@veeder.com>
2022-05-18 19:33:18 -0400

    added new project

---
+++ src/main/java/demoHibernate/App.java
@@ -0,0 +1,15 @@
+package demoHibernate;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        Alien gautam = new Alien();
+
+        gautam.setAid(101);;
+        gautam.setAname("Gautam");
+        gautam.setColor("Green");
+
+
+    }
+}
